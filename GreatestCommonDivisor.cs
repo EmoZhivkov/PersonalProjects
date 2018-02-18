dc3774842164682e03ee39f725ef076e122272c0
Emil Zhivkov <emo.zhivkov98@gmail.com>
2018-02-18 17:56:10 +0200

    adding a new problem

---
+++ GreatestCommonDivisor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Euclid's Algorithm
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int remainder = 0;
+            if (a >= b)
+            {
+                while (b != 0)
+                {
+                    remainder = a % b;
+                    a = b;
+                    b = remainder;
+
+                }
+                Console.WriteLine(a);
+            }
+            else
+            {
+                while (a != 0)
+                {
+                    remainder = b % a;
+                    b = a;
+                    a = remainder;
+
+                }
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
+
