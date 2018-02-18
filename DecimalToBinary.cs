12df647920a7937c7e989d017c2c8c25ab4f6aef
Emil Zhivkov <emo.zhivkov98@gmail.com>
2018-02-18 14:35:29 +0200

    Add DecimalToBinary.cs

---
+++ DecimalToBinary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+            int helper = n;
+            int h = 0;
+            while (helper > 0)
+            {
+                helper = helper / 2;
+                h++;
+            }
+
+            int[] arr = new int[h];
+            int cnt = 0;
+            while (n > 0)
+            {
+                if (n%2 == 0)
+                {
+                    arr[cnt] = 0;
+                }
+                else
+                {
+                    arr[cnt] = 1;
+                }
+                n = n / 2;
+                cnt++;
+            }
+
+            for (int i = h -1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+            
+        }   
+    }
+}
