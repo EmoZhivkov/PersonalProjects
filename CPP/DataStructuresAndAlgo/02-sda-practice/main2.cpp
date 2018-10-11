437601d06084d2758cf7ba393051291314f715a7
Emil Zhivkov <ezhivkov@uber.com>
2018-10-11 16:43:37 +0300

    extracted the sorting algorithms in a different folder

--- CPP/DataStructuresAndAlgo/02-sda-practice/main2.cpp
+++ CPP/DataStructuresAndAlgo/02-sda-practice/main2.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-#include <map>
-using namespace std;
-
-int main(){
-    string s;
-    cin >> s;
-
-    bool b = true;
-    map<char, int> count;
-
-    for (int i = 0; i < s.length(); ++i) {
-
-        if (count.count(s[i])) {
-
-            count[s[i]] = count[s[i]] + 1;
-
-        } else  {
-
-            count[s[i]] = 1;
-
-        }
-
-        if (s.length() % 2 == 0) {
-            if (count[s[i]] == (s.length() / 2) + 1) {
-
-                b = false;
-                break;
-
-            }
-        } else {
-            if (count[s[i]] == (s.length() / 2) + 2) {
-
-                b = false;
-                break;
-
-            }
-        }
-    }
-
-    if (b) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
-
-    return 0;
-}
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//
+//int main(){
+//    string s;
+//    cin >> s;
+//
+//    bool b = true;
+//    map<char, int> count;
+//
+//    for (int i = 0; i < s.length(); ++i) {
+//
+//        if (count.count(s[i])) {
+//
+//            count[s[i]] = count[s[i]] + 1;
+//
+//        } else  {
+//
+//            count[s[i]] = 1;
+//
+//        }
+//
+//        if (s.length() % 2 == 0) {
+//            if (count[s[i]] == (s.length() / 2) + 1) {
+//
+//                b = false;
+//                break;
+//
+//            }
+//        } else {
+//            if (count[s[i]] == (s.length() / 2) + 2) {
+//
+//                b = false;
+//                break;
+//
+//            }
+//        }
+//    }
+//
+//    if (b) {
+//        cout << "Yes" << endl;
+//    } else {
+//        cout << "No" << endl;
+//    }
+//
+//    return 0;
+//}
---
+++ CPP/DataStructuresAndAlgo/sorting-algo/bubble-selection-insertion.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int *arr, int lenght) {
+
+    for (int i = 0; i < lenght; ++i) {
+
+        int key = arr[i];
+        int j = i -1;
+
+        while (j >= 0 && arr[j] > key) {
+
+            arr[j + 1] = arr[j];
+
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(int *arr, int lenght) {
+    for (int i = 0; i < lenght; ++i) {
+
+        int smallest = i;
+
+        for (int j = i + 1; j < lenght; ++j) {
+
+            if (arr[j] < arr[smallest]) {
+
+                smallest = j;
+
+            }
+
+        }
+        swap(arr[i], arr[smallest]);
+    }
+}
+
+void bubbleSort(int *arr, int lenght) {
+
+    for (int i = 0; i < lenght; ++i) {
+
+        for (int j = 0; j < lenght - i - 1; ++j) {
+
+            if (arr[j] > arr[j + 1]) {
+
+               swap(arr[j], arr[j + 1]);
+
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[5] = {1,5,4,2,10};
+    insertionSort(arr, 5);
+
+    for (int i = 0; i < 5; ++i) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
