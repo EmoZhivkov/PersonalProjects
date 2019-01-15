a2fec66dffc7cdfc5014fc991cdfca6924103b44
Emil Zhivkov <emo.zhivkov98@gmail.com>
2019-01-16 00:57:07 +0200

    cpp hw

--- CPP/UP/03-hw/fn62215_d3_1.cpp
+++ CPP/UP/03-hw/fn62215_d3_1.cpp
@@ -12,12 +12,13 @@
 *
 */
 
-#include <iostream>
 #include <stdlib.h>
-
+#include <thread>
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     char matrix[5][40] = {
             "H   H   EEEEE   L       L          O   ",
             "H   H   E       L       L        O   O ",
@@ -27,42 +28,100 @@ int main() {
     };
 
     for (int j = 39; j >= 0; j--) {
-        //for (int i = 0; i < 40 - j; ++i) {
-            for (int k = 0; k < 10; ++k) {
-                cout << endl;
-            }
-
-            for (int l = j; l <= 40; ++l) {
-                cout << matrix[0][j];
-            }
+        for (int k = 0; k < 10; ++k) {
             cout << endl;
+        }
 
-            for (int l = j; l <= 40; ++l) {
-                cout << matrix[1][j];
-            }
+        for (int l = j; l < 40; ++l) {
+            cout << matrix[0][l];
+        }
+        cout << endl;
+
+        for (int l = j; l < 40; ++l) {
+            cout << matrix[1][l];
+        }
+        cout << endl;
+
+        for (int l = j; l < 40; ++l) {
+            cout << matrix[2][l];
+        }
+        cout << endl;
+
+        for (int l = j; l < 40; ++l) {
+            cout << matrix[3][l];
+        }
+        cout << endl;
+
+        for (int l = j; l < 40; ++l) {
+            cout << matrix[4][l];
+        }
+        cout << endl;
+
+        std::this_thread::sleep_for(chrono::milliseconds(50));
+
+        system("CLS");
+    }
+
+    for (int j = 0; j <= 39; j++) {
+        for (int k = 0; k < 10; ++k) {
             cout << endl;
+        }
 
-            for (int l = j; l <= 40; ++l) {
-                cout << matrix[2][j];
-            }
-            cout << endl;
+        for (int i = 0; i < j; i++)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < 39 - j; ++l) {
 
-            for (int l = j; l <= 40; ++l) {
-                cout << matrix[3][j];
-            }
-            cout << endl;
+            cout << matrix[0][l];
+        }
+        cout << endl;
 
-            for (int l = j; l <= 40; ++l) {
-                cout << matrix[4][j];
-            }
-            cout << endl;
+        for (int i = 0; i < j; i++)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < 39 - j; ++l) {
 
-            for (int k = 0; k < 10; ++k) {
-                cout << endl;
-            }
-        //}
+            cout << matrix[1][l];
+        }
+        cout << endl;
+
+        for (int i = 0; i < j; i++)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < 39 - j; ++l) {
+
+            cout << matrix[2][l];
+        }
+        cout << endl;
+
+        for (int i = 0; i < j; i++)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < 39 - j; ++l) {
+
+            cout << matrix[3][l];
+        }
+        cout << endl;
+
+        for (int i = 0; i < j; i++)
+        {
+            cout << " ";
+        }
+        for (int l = 0; l < 39 - j; ++l) {
+
+            cout << matrix[4][l];
+        }
+        cout << endl;
+
+        std::this_thread::sleep_for(chrono::milliseconds(50));
+
         system("CLS");
     }
 
     return 0;
 }
+
