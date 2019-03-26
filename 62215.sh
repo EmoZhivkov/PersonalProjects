e59ab87ac55b18471ed50c81c042d8c4804cb0e6
Emil Zhivkov <ezhivkov@uber.com>
2019-03-26 13:18:44 +0200

    bash homework

---
+++ 62215.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dic=( $(cat ~/Downloads/61000/dic.txt) )
+
+textByWords=( $( tr -cs '[:alpha:]' '\n' < ~/Downloads/61000/text.txt | awk '{print tolower($0)}' | sort | uniq ) )
+
+num=100
+dicIndex=0
+
+for (( i=0; i<${#textByWords[@]}; i++ ))
+do
+    for (( j=0; j<${#dic[@]}; j++ ))
+    do
+        temp=$( levenshtein ${dic[j]} ${textByWords[i]} )
+        if [[ ${num} -gt ${temp} ]]; then
+            dicIndex=${j}
+            num=${temp}
+        fi
+    done
+
+    if [[ ${num} != 0 ]]; then
+        echo ${textByWords[i]}:${num}:${dic[dicIndex]}
+    fi
+
+    num=100
+done
+
+echo ------------------
+
+tr -cs '[:alpha:]' '\n' < text.txt | awk '{print tolower($0)}' | sort | uniq | xargs -I{} -n1 sh -c '(echo {}: && agrep -w -B -s {} dic.txt) | tr "\n" ":"; echo "\n"' | awk -F: '{if ($3 != 0) print $1":"$3":"$4}' | grep -v "::"
