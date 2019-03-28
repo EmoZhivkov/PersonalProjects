e958fcf4d3e8f6e6ed3f35f9d243cbc51d39859e
Emil Zhivkov <ezhivkov@uber.com>
2019-03-28 13:22:41 +0200

    added the first os homework

--- 62215/62215.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-tr -cs '[:alpha:]' '\n' < text.txt | awk '{print tolower($0)}' | sort | uniq | \
-xargs -I{} -n1 sh -c '(echo {}: && agrep -w -B -s {} dic.txt) | tr "\n" ":"; echo "\n"'| \
-grep . | awk -F: '{if ($3 != 0) print $1":"$3":"$4}'
